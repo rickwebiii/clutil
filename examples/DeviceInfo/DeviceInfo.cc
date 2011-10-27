b4ff4d59564f2c84a163efb6bbba63f5ef146677
rick.weber.iii <rick.weber.iii@d036fdd0-62f0-0f51-e489-b651f146da92>
2011-10-27 05:41:16 +0000

    Updated device info example program.

--- examples/DeviceInfo/DeviceInfo.cc
+++ examples/DeviceInfo/DeviceInfo.cc
@@ -1,14 +1,25 @@
 #include <clUtil.h>
+#include <iostream>
+
+using namespace clUtil;
+using namespace std;
 
 int main(int argc, char** argv)
 {
-  clUtilInitDevices();
+  Device::FetchDevices();
+  size_t curDeviceNum = 0;
 
-  for(auto curDevice = clUtil::gDevices.begin();
-      curDevice != clUtil::gDevices.end();
+  for(auto curDevice = Device::Devices.begin();
+      curDevice != Device::Devices.end();
       curDevice++)
   {
-
+    cout << "Device " << curDeviceNum++ << endl;
+    cout << "\tName:" << (*curDevice).getDeviceInfo().Name << endl;
+    cout << "\tVendor:" << (*curDevice).getDeviceInfo().Vendor << endl;
+    cout << "\tDriver:" << (*curDevice).getDeviceInfo().DriverVersion << endl;
+    cout << "\tProfile:" << (*curDevice).getDeviceInfo().OpenCLProfile << endl;
+    cout << "\tVersion:" << (*curDevice).getDeviceInfo().OpenCLVersion << endl;
+    cout << "\tCVersion:" << (*curDevice).getDeviceInfo().OpenCLCVersion << endl;
   }
 
   return 0;
